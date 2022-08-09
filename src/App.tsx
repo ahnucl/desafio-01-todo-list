0c83bbcdcb5e9d171703314bab3073bb27341561
Leonardo Cunha <lalmcunha@gmail.com>
2022-08-09 19:56:16 -0300

    Cria markup inicial

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import { Header } from "./components/Header";
+import styles from './App.module.css'
+
 import './global.css'
 
 function App() {
@@ -6,6 +8,51 @@ function App() {
   return (
     <div>
       <Header />
+      
+      <input placeholder="Adicione uma tarefa" />
+      <button>Criar +</button>
+
+      <div>
+        Tarefas criadas
+        5
+      </div>
+
+      <div>
+        Concluídas
+        2 de 5
+      </div>
+
+      <div className={styles.todoList}>
+        <div>
+          <input type="checkbox" />
+          <div>Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.</div>
+          <button>X</button>
+        </div>
+
+        <div>
+          <input type="checkbox" />
+          <div>Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.</div>
+          <button>X</button>
+        </div>
+
+        <div>
+          <input type="checkbox" />
+          <div>Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.</div>
+          <button>X</button>
+        </div>
+
+        <div>
+          <input type="checkbox" checked/>
+          <div>Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.</div>
+          <button>X</button>
+        </div>
+
+        <div>
+          <input type="checkbox" checked/>
+          <div>Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.</div>
+          <button>X</button>
+        </div>
+      </div>
     </div>
   )
 }
